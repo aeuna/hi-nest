11b1671e3649ba4a9623e57ebd7019f41d489980
aeuna <4294psy@naver.com>
2021-01-04 23:16:02 +0900

    More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,24 +15,32 @@ export class MoviesController {
   getAll() {
     return 'This will return all movies';
   }
-  @Get('/:id')
+
+  @Get('search') //search가 id 아래에 있으면 search 를 id로 생각하기때문에 위로 뺌.
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after : ${searchingYear}`;
+  }
+
+  @Get(':id') //얘가 위에 있으면 다른 get들이 작동하지 않음.
   getOne(@Param('id') movieId: string) {
-    //주소의 parameter을 가져오고 싶으면, 요청을 해야한다.
     return `This will return one movies with the id : ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() createData) {
+    return createData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id : ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id : ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
