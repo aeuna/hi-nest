c3205d32ef81cc21a4ac01fab2b1801d1fc23629
aeuna <4294psy@naver.com>
2021-01-07 14:51:32 +0900

    DTOs and Validation part Two

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,10 +1,11 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator'; //유효성검사 라이브러리
 
 export class CreateMovieDto {
   @IsString()
   readonly title: string;
   @IsNumber()
   readonly year: number;
+  @IsOptional()
   @IsString({ each: true }) //각각의 요소를 하나씩 검사한다.
   readonly genres: string[];
 }
