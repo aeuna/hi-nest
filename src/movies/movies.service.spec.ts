53ab3f9d49250087753888a975b63b817d6e02ec
aeuna <4294psy@naver.com>
2021-01-08 21:10:05 +0900

    First Unit Test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
+  //npm run test:watch
   let service: MoviesService;
 
   beforeEach(async () => {
+    //테스트를 실행하기전에 행동
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
     }).compile();
@@ -13,6 +15,11 @@ describe('MoviesService', () => {
   });
 
   it('should be defined', () => {
+    //it => individual test
     expect(service).toBeDefined();
+  });
+
+  it('should be 4', () => {
+    expect(2 + 2).toEqual(4);
   });
 });
