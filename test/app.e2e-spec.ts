5731febc39443c3988bd741b78a697d68dbe0950
aeuna <4294psy@naver.com>
2021-01-11 21:59:05 +0900

    Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,16 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    //테스트를 실행할때마다 어플리케이션을 생성하지 않도록
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication(); //어떤 pipe로도 감싸지지 않았기 때문에 getOne으로 id 를 가질때, string 으로 타입이 나타남
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, //이상한거 빼고 정보 전달.
+        forbidNonWhitelisted: true, //이상한 request를 아예 막음.
+        transform: true, //우리가 원하는 타입으로 변환. 타입을 받아서 넘겨줄때 자동으로 타입변환
+      }),
+    ); //유효성 검사 실시.
     await app.init();
   });
 
@@ -41,5 +50,13 @@ describe('AppController (e2e)', () => {
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it.todo('DELETE');
+    it.todo('Patch');
   });
 });
